872ee14020743aa41ba7ebc7c2819eaef149293a
Aya AbdulSlam <ahemaid@integrant.com>
2021-05-16 01:57:19 +0200

    Initial Create

---
+++ TemplateDesignPattern/Products.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Text;
+
+namespace TemplateDesignPattern
+{
+    internal class Products : DataAccessObject
+
+    {
+        public override void Select()
+        {
+            string sql = "select ProductName from Products";
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(
+              sql, connectionString);
+
+            dataSet = new DataSet();
+            dataAdapter.Fill(dataSet, "Products");
+        }
+
+        public override void Process()
+        {
+            Console.WriteLine("Products ---- ");
+            DataTable dataTable = dataSet.Tables["Products"];
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine(row["ProductName"]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
