8048fd1476812011855b51408e44d49f1b05ffcd
Aya AbdulSlam <ahemaid@integrant.com>
2021-05-16 02:07:02 +0200

    Adding Another Example

---
+++ BreadTemplateDesignPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BreadTemplateDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bread sourdough = new Sourdough();
+            sourdough.Make();
+
+            Console.WriteLine("\n");
+            Bread twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+
+            Console.WriteLine("\n");
+
+            Bread wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+            Console.WriteLine("\n");
+
+            Console.ReadKey();
+        }
+    }
+}
